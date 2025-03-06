5080068d6568d9af702467976527aa1d63eb1fb8
Kris Mehta <krismehta02@gmail.com>
2025-03-06 06:57:17 +0530

    Update A1Q1.java

--- OOPJ/Assignment1/A1Q1.java
+++ OOPJ/Assignment1/A1Q1.java
@@ -1,1 +1,21 @@
-This is Assingment 1
+public class A1Q1{
+    public static void main(String[] args) {
+        byte byteValue = -120;
+        short shortValue = 15000;
+        int intValue = 500000;
+        long longValue = 1234567890L;
+        float floatValue = 7.89f;
+        double doubleValue = 6.283185307179586;
+        char charValue = 'Z';
+        boolean booleanValue = false;
+
+        System.out.println("byte value: " + byteValue);
+        System.out.println("short value: " + shortValue);
+        System.out.println("int value: " + intValue);
+        System.out.println("long value: " + longValue);
+        System.out.println("float value: " + floatValue);
+        System.out.println("double value: " + doubleValue);
+        System.out.println("char value: " + charValue);
+        System.out.println("boolean value: " + booleanValue);
+    }
+}
