80cdada3035c14ed45640d86c2575da29b6bea26
Kris Mehta <krismehta02@gmail.com>
2025-03-07 19:51:47 +0530

    Update A1Q4.java

--- OOPJ/Assignment1/A1Q4.java
+++ OOPJ/Assignment1/A1Q4.java
@@ -1,1 +1,16 @@
+public class A1Q4 {
+    public static void main(String[] args) {
+        int x = 22;
+
+        Integer OBJX = Integer.valueOf(x);
+        double OBJY = OBJX.doubleValue();
+
+        double Z = 25.25;
+        Double OBJZ = Double.valueOf(Z);
+        int a = OBJZ.intValue();
+
+        System.out.println("int : " + x + " to Double is : " + OBJY);
+        System.out.println("Double value : " + Z + " to int is : " + a);
+    }
+}
 
