2e781ad9b09ee1549bd3a08608484b54cb614750
Kris Mehta <krismehta02@gmail.com>
2025-03-07 21:28:00 +0530

    Update A1Q7.java

--- OOPJ/Assignment1/A1Q7.java
+++ OOPJ/Assignment1/A1Q7.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class A1Q6 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number to check whether it is odd or even:");
+
+        int x = input.nextInt();
+
+        System.out.println(x + (x % 2 == 0 ? " is even" : " is odd"));
+
+        input.close();
+    }
+}
 
