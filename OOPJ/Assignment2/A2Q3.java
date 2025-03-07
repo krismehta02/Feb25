af8a5a9a1bfb0e0b5879eb6d24fd1a91053df920
Kris Mehta <krismehta02@gmail.com>
2025-03-07 23:02:17 +0530

    Update A2Q3.java

--- OOPJ/Assignment2/A2Q3.java
+++ OOPJ/Assignment2/A2Q3.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter an integer to calculate the sum of its digits:");
+        
+        int num = input.nextInt();
+        int sum = 0;
+
+        while (num != 0) {
+            sum += num % 10;  
+            num = num / 10;   
+        }
+
+        System.out.println("Sum of the digits: " + sum);
+    }
+}
 
