3330499c413a4ec0146fa09868185598dfb5b523
Kris Mehta <krismehta02@gmail.com>
2025-03-07 20:50:31 +0530

    Update A1Q5.java

--- OOPJ/Assignment1/A1Q5.java
+++ OOPJ/Assignment1/A1Q5.java
@@ -1,1 +1,27 @@
+public class A1Q5 {
+    public static void main(String[] args) {
+
+        int x = 20;
+        int y = 30;
+
+        System.out.println("Value of X and Y before swapping using 3rd variable: " + x + " and " + y);
+
+        int z = x;
+        x = y;
+        y = z;
+
+        System.out.println("Value of X and Y after swapping using 3rd variable: " + x + " and " + y);
+
+        x = 20;
+        y = 30;
+
+        System.out.println("Value of X and Y before swapping without using 3rd variable: " + x + " and " + y);
+
+        x = x * y;
+        y = x / y;
+        x = x / y;
+
+        System.out.println("Value of X and Y after swapping without using 3rd variable: " + x + " and " + y);
+    }
+}
 
