7a2fe3597496826e123edc24faae669f4e92c1c3
Kris Mehta <krismehta02@gmail.com>
2025-03-07 09:53:21 +0530

    Update A1Q3.java

--- OOPJ/Assignment1/A1Q3.java
+++ OOPJ/Assignment1/A1Q3.java
@@ -1,1 +1,16 @@
+public class A1Q3 {
+    public static void main(String[] args) {
+        byte Num1 = 10;
+        float Num2 = 2828.28f;
+
+        int Num3 = Num1;
+
+        int Num4 = (int) Num2;
+
+        System.out.println("Byte number is : " + Num1);
+        System.out.println("Implicit int from byte number is : " + Num3);
+        System.out.println("Float number is : " + Num2);
+        System.out.println("Explicit int from float number: " + Num4);
+    }
+}
 
