10017a6ab380d854998374cb3df2d5e13df080f3
Kris Mehta <krismehta02@gmail.com>
2025-03-07 09:32:06 +0530

    Update A1Q2.java

--- OOPJ/Assignment1/A1Q2.java
+++ OOPJ/Assignment1/A1Q2.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class A1Q2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number 1:");
+        int num1 = input.nextInt();
+
+        System.out.println("Enter number 2:");
+        int num2 = input.nextInt();
+
+        System.out.println("Addition of given numbers: " + (num1 + num2));
+        System.out.println("Subtraction of given numbers: " + (num1 - num2));
+        System.out.println("Product of given numbers: " + (num1 * num2));
+        
+        if (num2 != 0) {
+            System.out.println("Division of given numbers: " + (num1 / num2));
+            System.out.println("Remainder of given numbers: " + (num1 % num2));
+        } else {
+            System.out.println("Division or remainder by zero is not possible.");
+        }
+
+        input.close();
+    }
+}
