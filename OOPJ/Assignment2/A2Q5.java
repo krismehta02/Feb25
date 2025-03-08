796426160b470e9e10b58fc69c6c6cf50c44c55e
Kris Mehta <krismehta02@gmail.com>
2025-03-08 07:20:58 +0530

    Update A2Q5.java

--- OOPJ/Assignment2/A2Q5.java
+++ OOPJ/Assignment2/A2Q5.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter the first number:");
+        int a = input.nextInt();
+        
+        System.out.println("Enter the second number:");
+        int b = input.nextInt();
+        
+        a += b;
+        b = a - b;
+        a -= b;
+        
+        System.out.println("After swapping: ");
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+    }
+}
 
