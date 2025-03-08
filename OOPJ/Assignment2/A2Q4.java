22d9f79e888e54c29a4b66b34e7911532806e709
Kris Mehta <krismehta02@gmail.com>
2025-03-08 07:14:29 +0530

    Update A2Q4.java

--- OOPJ/Assignment2/A2Q4.java
+++ OOPJ/Assignment2/A2Q4.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class DivisibleBy3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter a number to check if it is divisible by 3:");
+        int num = input.nextInt();
+        
+        while (num >= 3) {
+            num = num - 3;
+        }
+        
+        if (num == 0) {
+            System.out.println("The number is divisible by 3.");
+        } else {
+            System.out.println("The number is not divisible by 3.");
+        }
+    }
+}
 
