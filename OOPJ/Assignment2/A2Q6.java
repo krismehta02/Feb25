49c3139b31cf036e2c5b4b33cfebd50eb1224a44
Kris Mehta <krismehta02@gmail.com>
2025-03-08 18:49:58 +0530

    Update A2Q6.java

--- OOPJ/Assignment2/A2Q6.java
+++ OOPJ/Assignment2/A2Q6.java
@@ -1,1 +1,18 @@
+mport java.util.Scanner;
 
+public class A2Q6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+        int y = input.nextInt();
+        int z = input.nextInt();
+
+        String result = (x > y && x > z) ? x + " is the greatest" : 
+                        (y > z) ? y + " is the greatest" : 
+                        z + " is the greatest";
+
+        System.out.println(result);
+        input.close();
+    }
+}
