3b5405d38aa8df4f0b37c32ef438174c14da1e98
Kris Mehta <krismehta02@gmail.com>
2025-03-10 06:57:29 +0530

    Update and rename A2Qu10 to A2Qu10.java

---
+++ OOPJ/Assignment2/A2Qu10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+char ch = scanner.nextcharAr();
+
+string result = (char =='A' || char =='E' || char =='I' || char =='O' || char =='U' || char =='a' || char =='e || char =='i' || char =='o' || char =='u'? char + "is a vowel: char + " is a consonant")
+
+System.out.println(result);
+
+
+}
+}
