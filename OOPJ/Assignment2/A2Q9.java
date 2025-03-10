293e9d6046ae4a64e61bd18f88b8c7131c60f6ec
Kris Mehta <krismehta02@gmail.com>
2025-03-10 06:38:33 +0530

    Update A2Q9.java

--- OOPJ/Assignment2/A2Q9.java
+++ OOPJ/Assignment2/A2Q9.java
@@ -1,1 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 25;
 
+        String range = (x > 19 && x < 51) ? x + " is in range" : x + " is out of range";
+
+        System.out.println(range);
+    }
+}
