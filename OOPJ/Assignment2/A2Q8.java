adf2bf847669e0e6b53d01a1648abc429f5f3c65
Kris Mehta <krismehta02@gmail.com>
2025-03-10 06:07:39 +0530

    Update A2Q8.java

--- OOPJ/Assignment2/A2Q8.java
+++ OOPJ/Assignment2/A2Q8.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean a = scanner.nextBoolean();
+        boolean b = scanner.nextBoolean();
+        boolean c = scanner.nextBoolean();
+
+        if ((a && b) || (a && c) || (b && c)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        scanner.close();
+    }
+}
+
 
